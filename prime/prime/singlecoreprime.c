25d477cb5e11aefb100bc0280baa155e45521cea
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2021-09-17 10:15:08 -0400

    Update singlecoreprime.c
    this one compiles and runs but it's iterating over the numbers too many times so the array needs to be defined as yuge (like 999999999)!

--- prime/prime/singlecoreprime.c
+++ prime/prime/singlecoreprime.c
@@ -1,28 +1,36 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <time.h>
 #include <math.h>
-
+///int primes[]={1,2};
 int main(int argc, char *argv[]){
-    int num = atoi(argv[1]);
-    int i,j;
-    float square;
+int num = atoi(argv[1]);
+    int i,j,v;
+    int p=2;
+    int square;
+    int primes[]={1,2};
     clock_t start = clock();
-    for (i=2;i<num;++i) 
+    for (i=3;i<num;i+=2)
     {
         square=sqrt(i);
-        for (j=2;j<square;++j) 
+        for (j=3;j<(square+1);j+=2)
             {
-            if (i%j==0) 
+            if (i%j==0)
                 {
-                break; 
+                break;
                 }
-        else 
+        else
             {
-            printf("%d is a prime \n",i); 
+            printf("%d is a prime \n",i);
+            ++p;
+            primes[p]=i;
             }
         }
     }
     clock_t stop = clock();
+    for (v=(p-100);v<p;++v)
+        {
+        printf("%d \n",primes[v]);
+        }
     printf("--- %f seconds ___\n", (double)(start-stop)/CLOCKS_PER_SEC);
 }
