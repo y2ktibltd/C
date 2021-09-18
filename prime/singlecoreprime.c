63ab7306ab90107b3a08aeca46cacdc26b21ab73
Tom Bigham <75551001+y2ktibltd@users.noreply.github.com>
2021-09-18 13:29:27 -0400

    Update singlecoreprime.c

--- prime/singlecoreprime.c
+++ prime/singlecoreprime.c
@@ -2,36 +2,20 @@
 #include <stdio.h>
 #include <time.h>
 #include <math.h>
-///int primes[]={1,2};
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
 int num = atoi(argv[1]);
-    int i,j,v;
-    int p=2;
-    int square;
-    int primes[]={2};
-    clock_t start = clock();
-    for (i=3;i<num;i+=2)
+int i,j,square;
+clock_t start = clock();
+printf("2, ");
+for(i=3;i<num;i+=2)
     {
-        printf("%d loops\n",i);
-        square=sqrt(i);
-        for (j=3;j<(square+1);j+=2)
-            {
-            if (i%j==0)
-                {
-                break;
-                }
-        else
-            {
-            printf("%d is a prime \n",i);
-            ++p;
-            primes[p]=i;
-            }
-        }
+    square=(sqrt(i)+1);
+    for(j=2;j<=square;j++)
+    if(!(i%j)) break; 
+    if(j>square) printf("%d, ", i);
     }
-    clock_t stop = clock();
-    for (v=(p-100);v<p;++v)
-        {
-        printf("%d \n",primes[v]);
-        }
-    printf("--- %f seconds ___\n", (double)(start-stop)/CLOCKS_PER_SEC);
+clock_t stop = clock();
+printf("\n--- %f seconds ___\n", (double)(start-stop)/CLOCKS_PER_SEC);
+return 0;
 }
